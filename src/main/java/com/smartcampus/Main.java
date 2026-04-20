3bc92ab6f5a2201eaf6f6711ed5ad228c4fe22f6
Yeheni-Bit <alwisyeheni@gmail.com>
2026-04-20 12:27:12 +0530

    Fixed Part 1 setup - API running with correct configuration

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus;
 
+import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.net.URI;
-
 public class Main {
 
-    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_URI = "http://localhost:8081/api/v1/";
 
     public static HttpServer startServer() {
-        final ResourceConfig config = new ResourceConfig() 
+        final ResourceConfig config = new ResourceConfig()
                 .packages("com.smartcampus");
 
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
@@ -23,7 +18,7 @@ public class Main {
 
     public static void main(String[] args) {
         HttpServer server = startServer();
-        System.out.println("Smart Campus API running at http://localhost:8080/api/v1/");
+        System.out.println("Smart Campus API running at http://localhost:8081/api/v1/");
         System.out.println("Press Ctrl + C to stop.");
 
         try {
---
+++ src/main/java/com/smartcampus/resource/RootResource.java
@@ -0,0 +1,30 @@
+package com.smartcampus.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class RootResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getApiInfo() {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("name", "Smart Campus API");
+        response.put("version", "v1");
+        response.put("contact", "yourname@westminster.ac.uk");
+
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+
+        response.put("resources", resources);
+
+        return response;
+    }
+}
